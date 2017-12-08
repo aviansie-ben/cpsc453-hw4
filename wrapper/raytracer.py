97e257cf3ed6f46d53b2d67e9bd9cbef95601f09
Benjamin Thomas (Aviansie Ben) <ben@benthomas.ca>
2017-12-08 03:49:46 +0000

    Implement a very simple Python wrapper
    While it is best to run the ray tracer directly for maximum control over
    its parameters, the assignment requires that the program support the
    flags --yours and --default. These flags are overly complicated to
    implement in the actual ray tracer, since they set many options, so a
    Python wrapper script has been provided that translates these two
    options into actual options used by the ray tracer.

---
+++ wrapper/raytracer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from pathlib import Path
+import subprocess
+import sys
+
+def main():
+    p = Path(__file__).parent
+    if not Path(p, 'build/hw4').exists():
+        print('The program must be built by running "make" first', file=sys.stderr)
+        sys.exit(127)
+
+    if len(sys.argv) == 2 and sys.argv[1] == '--yours':
+        subprocess.run([
+            Path(p, 'build/hw4'),
+            '--no-preview',
+            '--supersample',
+            '4',
+            '-s',
+            '512,512',
+            '-o',
+            'yours.ppm',
+            Path(p, '../scenes/knight.scn')
+        ])
+    elif len(sys.argv) == 2 and sys.argv[1] == '--default':
+        # TODO: Implement this
+        print('Not yet implemented', file=sys.stderr)
+    else:
+        print('Usage: {} {{ --yours | --default }}'.format(sys.argv[0]), file=sys.stderr)
+
+if __name__ == '__main__':
+    main()
